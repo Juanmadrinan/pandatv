8626ca2674c63baed33b2d55ed43cb90c9cdd7bc
Jmadrinanpinzon@gmail.com <juan.madrinanjp@est.iudigital.edu.co>
2024-02-26 09:26:02 -0500

    Actualizacion index.js a server.js, con ruta de iniciar aplicacion en el puerto 3000

---
+++ server.js
@@ -0,0 +1,14 @@
+
+const express = require('express');
+const app = express();
+const port = 3000;
+
+// Ruta de ejemplo
+app.get('/', (req, res) => {
+  res.send('¡Hola, mundo!');
+});
+
+// Inicia el servidor
+app.listen(port, () => {
+  console.log(`Running: http://localhost:${port}`);
+});
