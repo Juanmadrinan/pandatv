1b48354cb1f6fba952dd42ece34f93eb4a27911c
ElkevinM <kevin.monsalvem@est.iudigital.edu.co>
2024-03-22 10:16:55 -0500

    codigo de nav bar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
+import './styles/App.css';
+import Navbar from './components/navbar';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-       <p>esto es un ejemplo</p>
-      </header>
+      <Navbar/>
     </div>
   );
 }
